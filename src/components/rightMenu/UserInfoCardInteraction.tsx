6efbdf78f2513423bbaed1c73279ca3218893d11
Andrey Kruglik <a.kruglik@keep-calm.ru>
2024-09-29 22:57:52 +0300

    added follow function with optimistic state

--- src/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { switchFollow } from '@/lib/actions';
+import { useOptimistic, useState } from 'react';
+
 type UserInfoCardInteractionProps = {
   userId: string;
   currentUserId: string;
@@ -15,16 +18,45 @@ const UserInfoCardInteraction = ({
   isFollowing,
   isFollowingSent,
 }: UserInfoCardInteractionProps) => {
+  const [userState, setUserState] = useState({
+    blocked: isUserBlocked,
+    following: isFollowing,
+    followingRequestSent: isFollowingSent,
+  });
+  const [optimisticFollow, setOptimisticFollow] = useOptimistic(userState, state => ({
+    ...state,
+    following: state.following && false,
+    followingRequestSent: !state.following && !state.followingRequestSent,
+  }));
+
+  const follow = async () => {
+    setOptimisticFollow('');
+    try {
+      await switchFollow(userId);
+      setUserState(prevState => ({
+        ...prevState,
+        following: prevState.following && false,
+        followingRequestSent: !prevState.following && !prevState.followingRequestSent,
+      }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <>
-      <form action=''>
+      <form action={follow}>
         <button type={'submit'} className={'w-full bg-blue-500 text-white text-sm rounded-md p-2'}>
-          {isFollowing ? 'Following' : isFollowingSent ? 'Friend Request Sent' : 'Follow Request'}
+          {optimisticFollow.following
+            ? 'Following'
+            : optimisticFollow.followingRequestSent
+              ? 'Friend Request Sent'
+              : 'Follow'}
         </button>
       </form>
       <form action='' className={'self-end'}>
         <button type={'submit'} className={'text-red-400 text-xs cursor-pointer'}>
-          {isUserBlocked ? 'Unblock User' : 'Block User'}
+          {optimisticFollow.blocked ? 'Unblock User' : 'Block User'}
         </button>
       </form>
     </>
