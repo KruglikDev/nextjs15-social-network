5f103247b679a5f4367f6a38ef196ec560ad5d48
Andrey Kruglik <a.kruglik@keep-calm.ru>
2024-10-02 21:17:14 +0300

    added loading media from server in UserMediaCard

--- src/components/rightMenu/UserMediaCard.tsx
+++ src/components/rightMenu/UserMediaCard.tsx
@@ -1,41 +1,22 @@
+import prisma from '@/lib/client';
 import type { User } from '@prisma/client';
 import Image from 'next/image';
 import Link from 'next/link';
 
-const usersPhotos = [
-  {
-    id: '01',
-    imgLink:
-      'https://images.unsplash.com/photo-1560743173-567a3b5658b1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-  {
-    id: '02',
-    imgLink:
-      'https://images.unsplash.com/photo-1560743173-567a3b5658b1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-  {
-    id: '03',
-    imgLink:
-      'https://images.unsplash.com/photo-1560743173-567a3b5658b1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-  {
-    id: '04',
-    imgLink:
-      'https://images.unsplash.com/photo-1560743173-567a3b5658b1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-  {
-    id: '05',
-    imgLink:
-      'https://images.unsplash.com/photo-1560743173-567a3b5658b1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-  {
-    id: '06',
-    imgLink:
-      'https://images.unsplash.com/photo-1560743173-567a3b5658b1?q=80&w=1469&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  },
-];
+const UserMediaCard = async ({ user }: { user: User }) => {
+  const postWithMedia = await prisma.post.findMany({
+    where: {
+      userId: user.id,
+      img: {
+        not: null,
+      },
+    },
+    take: 8,
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
 
-const UserMediaCard = ({ user }: { user: User }) => {
   return (
     <section className={'p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4'}>
       {/*TOP*/}
@@ -46,12 +27,14 @@ const UserMediaCard = ({ user }: { user: User }) => {
         </Link>
       </div>
       {/*BOTTOM*/}
-      <div className={'flex gap-4 justify-between flex-wrap'}>
-        {usersPhotos.map(photo => (
-          <div key={photo.id} className={'relative w-1/4 h-24'}>
-            <Image src={photo.imgLink} alt={'user"s photo'} fill className={'object-cover rounded-md'} />
-          </div>
-        ))}
+      <div className={'flex gap-4 flex-wrap'}>
+        {postWithMedia.length
+          ? postWithMedia.map(post => (
+              <div key={post.id} className={'relative w-1/4 h-24'}>
+                {post.img && <Image src={post.img} alt={post.desc} fill className={'object-cover rounded-md'} />}
+              </div>
+            ))
+          : 'No media found'}
       </div>
     </section>
   );
