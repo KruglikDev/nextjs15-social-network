fc48554e2860e20e21cb63cea0e1e6935d78da50
Andrey Kruglik <a.kruglik@keep-calm.ru>
2024-10-02 23:51:14 +0300

    added acceptFollowRequest

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -58,3 +58,33 @@ export const switchBlock = async (userId: string) => {
     throw new Error('Something went wrong');
   }
 };
+
+export const acceptFollowRequest = async (userId: string) => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) throw new Error('User is not authenticated');
+
+  try {
+    const existingRequest = await prisma.followRequest.findFirst({
+      where: { senderId: userId, receiverId: currentUserId },
+    });
+
+    if (existingRequest) {
+      await prisma.followRequest.delete({
+        where: {
+          id: existingRequest.id,
+        },
+      });
+
+      await prisma.follower.create({
+        data: {
+          followerId: userId,
+          followingId: currentUserId,
+        },
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    throw new Error('Something went wrong');
+  }
+};
