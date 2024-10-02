ef3e16f1c530662de295a28c364f5916ab6784dc
Andrey Kruglik <a.kruglik@keep-calm.ru>
2024-10-03 00:26:10 +0300

    added accept and decline follower functionality to FriendRequestList

--- src/components/rightMenu/FriendRequestList.tsx
+++ src/components/rightMenu/FriendRequestList.tsx
@@ -1,16 +1,43 @@
 'use client';
 
+import { acceptFollowRequest, declineFollowRequest } from '@/lib/actions';
 import type { FollowRequest, User } from '@prisma/client';
 import Image from 'next/image';
+import { useOptimistic, useState } from 'react';
 
 type FriendRequestListProps = FollowRequest & {
   sender: User;
 };
 
 const FriendRequestList = ({ requests }: { requests: FriendRequestListProps[] }) => {
+  const [requestState, setRequestState] = useState(requests);
+  const [optimisticRequests, removeOptimisticRequest] = useOptimistic(requestState, (state, value: number) =>
+    state.filter(req => req.id !== value),
+  );
+
+  const acceptRequest = async (requestId: number, userId: string) => {
+    removeOptimisticRequest(requestId);
+    try {
+      await acceptFollowRequest(userId);
+      setRequestState(prev => prev.filter(req => req.id !== requestId));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const declineRequest = async (requestId: number, userId: string) => {
+    removeOptimisticRequest(requestId);
+    try {
+      await declineFollowRequest(userId);
+      setRequestState(prev => prev.filter(req => req.id !== requestId));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
-      {requests.map(request => {
+      {optimisticRequests.map(request => {
         const username =
           request?.sender?.name && request?.sender?.surname
             ? `${request?.sender?.name} ${request?.sender?.surname}`
@@ -29,8 +56,16 @@ const FriendRequestList = ({ requests }: { requests: FriendRequestListProps[] })
               <span className={'font-semibold'}>{username}</span>
             </div>
             <div className={'flex gap-3 justify-end'}>
-              <Image src={'/accept.png'} width={20} height={20} alt={'accept icon'} className={'cursor-pointer'} />
-              <Image src={'/reject.png'} width={20} height={20} alt={'reject icon'} className={'cursor-pointer'} />
+              <form action={() => acceptRequest(request.id, request?.sender?.id)}>
+                <button type={'submit'}>
+                  <Image src={'/accept.png'} width={20} height={20} alt={'accept icon'} className={'cursor-pointer'} />
+                </button>
+              </form>
+              <form action={() => declineRequest(request.id, request?.sender?.id)}>
+                <button type={'submit'}>
+                  <Image src={'/reject.png'} width={20} height={20} alt={'reject icon'} className={'cursor-pointer'} />
+                </button>
+              </form>
             </div>
           </div>
         );
