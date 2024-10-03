4f9e84539e448e32a46b5dcce92640f0cdde722e
Andrey Kruglik <a.kruglik@keep-calm.ru>
2024-10-03 22:51:08 +0300

    added zod package and updateProfile action

--- src/components/rightMenu/UpdateUser.tsx
+++ src/components/rightMenu/UpdateUser.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { updateProfile } from '@/lib/actions';
 import type { User } from '@prisma/client';
 import Image from 'next/image';
 import { useRef } from 'react';
@@ -19,7 +20,7 @@ const UpdateUser = ({ user }: { user: User }) => {
         className={'top-0 left-0 z-50 backdrop:bg-black backdrop:bg-opacity-65 rounded-lg w-full md:w-1/2 xl:w-1/3'}
         ref={dialog}
       >
-        <form action='' className={'p-12 bg-white flex flex-col gap-2 '}>
+        <form action={updateProfile} className={'p-12 bg-white flex flex-col gap-2 '}>
           <h1>Update Profile</h1>
           <div className={'mt-4 text-xs text-gray-500'}>Use the navbar profile to change the avatar or username.</div>
 
@@ -47,6 +48,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.name || 'John'}
+                name={'name'}
               />
             </div>
 
@@ -59,6 +61,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.surname || 'Doe'}
+                name={'surname'}
               />
             </div>
 
@@ -71,6 +74,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.description || 'Example...'}
+                name={'description'}
               />
             </div>
 
@@ -83,6 +87,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.city || 'Chicago'}
+                name={'city'}
               />
             </div>
 
@@ -95,6 +100,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.school || 'Alaska High School'}
+                name={'school'}
               />
             </div>
 
@@ -107,6 +113,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.work || 'IBM Inc.'}
+                name={'work'}
               />
             </div>
 
@@ -119,6 +126,7 @@ const UpdateUser = ({ user }: { user: User }) => {
                 className={'ring-1 ring-gray-300 p-[13px] rounded-md text-sm'}
                 type='text'
                 placeholder={user?.website || 'lama.dev'}
+                name={'website'}
               />
             </div>
           </div>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,6 +2,7 @@
 
 import prisma from '@/lib/client';
 import { auth } from '@clerk/nextjs/server';
+import { z } from 'zod';
 
 export const switchFollow = async (userId: string) => {
   const { userId: currentUserId } = auth();
@@ -109,5 +110,40 @@ export const declineFollowRequest = async (userId: string) => {
   } catch (err) {
     console.error(err);
     throw new Error('Something went wrong');
+  }
+};
+
+export const updateProfile = async (formData: FormData) => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) throw new Error('User is not authenticated');
+
+  const fields = Object.fromEntries(formData);
+
+  const Profile = z.object({
+    cover: z.string().optional(),
+    name: z.string().max(60).optional(),
+    surname: z.string().max(60).optional(),
+    description: z.string().max(255).optional(),
+    city: z.string().max(60).optional(),
+    school: z.string().max(60).optional(),
+    work: z.string().max(60).optional(),
+    website: z.string().max(60).optional(),
+  });
+
+  const validatedFields = Profile.safeParse(fields);
+
+  if (!validatedFields.success) {
+    console.log(validatedFields.error.flatten().fieldErrors);
+    return null;
+  }
+
+  try {
+    await prisma.user.update({
+      where: { id: currentUserId },
+      data: validatedFields.data,
+    });
+  } catch (err) {
+    console.error(err);
   }
 };
